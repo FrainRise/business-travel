789903cda1ebbbb47930e2473a1e07d4e15c8502
FrainRise <frainrise@gmail.com>
2020-03-10 12:50:20 +0200

    Fixed warnings in the Navbar component

--- src/components/HomePage/home-components/Navbar.js
+++ src/components/HomePage/home-components/Navbar.js
@@ -16,13 +16,13 @@ export default function Navbar(){
             <div className="collapse navbar-collapse justify-content-between" id="navbarNav">
                 <ul className="navbar-nav mx-auto">
                     <li className="nav-item ml-5">
-                        <Link to='/' style={linkStyle}><a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a></Link>
+                        <Link to='/' style={linkStyle}><a className="nav-link" href="/#">Home <span className="sr-only">(current)</span></a></Link>
                     </li>
                     <li className="nav-item ml-5">
-                        <Link to='/about-us' style={linkStyle}><a className="nav-link" href="#">About</a></Link>
+                        <Link to='/about-us' style={linkStyle}><a className="nav-link" href="/#">About</a></Link>
                     </li>
                     <li className="nav-item ml-5">
-                        <Link to='/travels' style={linkStyle}><a className="nav-link" href="#">Travel</a></Link>
+                        <Link to='/travels' style={linkStyle}><a className="nav-link" href="/#">Travel</a></Link>
                     </li>
                 </ul>
             </div>

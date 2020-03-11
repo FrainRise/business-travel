6211224a0074e105b8e00929e52a2ae266bd60a8
FrainRise <frainrise@gmail.com>
2020-03-11 14:35:00 +0200

    Added adaptivity for Homepage component - laptops and small resolution desktop's screens

--- src/components/HomePage/home-components/Contact.js
+++ src/components/HomePage/home-components/Contact.js
@@ -8,19 +8,19 @@ export default class Contact extends React.Component {
             <div className="contact-section">
                 <div className="contact-info">
                     <div className="row">
-                        <div className="col col-md-8">
+                        <div className="col col-md-8 col-xl">
                             <h2 className="contact-title"> Book a discounter tour</h2>
                             <p className="descr">
                                 Mauris ultrices eros in cursus turpis 
                             </p>
                         </div>
-                        <div className="col col-md-6">
+                        <div className="col col-md-6 col-xl-3">
                             <input type="text" className="contact-input" placeholder="Name" />
                         </div>
-                        <div className="col col-md-6">
+                        <div className="col col-md-6 col-xl-3">
                             <input type="text" className="contact-input" placeholder="Email" />
                         </div>
-                        <div className="col col-md-6">
+                        <div className="col col-md-6 col-xl-4">
                             <button className="contact-btn">
                                 Send
                             </button>

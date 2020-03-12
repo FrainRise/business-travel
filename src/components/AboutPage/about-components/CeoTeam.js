1e64026171db04603b7fd4400b064063f6d9bd03
FrainRise <frainrise@gmail.com>
2020-03-12 12:27:34 +0200

    Added media-queries  for AboutPage component to make adaptivity for tablets and small laptops devices

--- src/components/AboutPage/about-components/CeoTeam.js
+++ src/components/AboutPage/about-components/CeoTeam.js
@@ -12,10 +12,10 @@ export default function CeoTeam() {
                 <p className="description">1 frontend developer, 1 backend developer, 3 photographers and infinity of friendship and kindness</p>
             </div>
             <div className="row custom-row">
-                <div className="col">
+                <div className="col col-lg-12">
                     <img src={andrewIcon} alt=""/>
                 </div>
-                <div className="col-10 text-col">
+                <div className="col-10 col-lg-9 text-col">
                     <h2 className="ceo-name">Andrew Kovalenko</h2>
                     <h6 className="ceo-position">Frontend developer</h6>
                     <p className="ceo-descr">
@@ -26,10 +26,10 @@ export default function CeoTeam() {
                 </div>
             </div>
             <div className="row custom-row">
-                <div className="col">
+                <div className="col col-lg-12">
                     <img src={clayIcon} alt="" />
                 </div>
-                <div className="col-10 text-col">
+                <div className="col-10 col-lg-9 text-col">
                     <h2 className="ceo-name">Clay Jenkins (a.k.a CJ)</h2>
                     <h6 className="ceo-position">Backend developer </h6>
                     <p className="ceo-descr">
